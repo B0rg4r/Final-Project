52e34f80904de84024031d4b2b2e464bfe71e526
Bl4nk3d <destruction379@gmail.com>
2026-04-28 19:14:19 +0100

    Started Interaction System
    Added an interactor and started code for the player with a raycast to interact with objects
    
    Interactor Event Script will allow all interactable to be added to the system

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerCam : MonoBehaviour
@@ -28,5 +29,21 @@ public class PlayerCam : MonoBehaviour
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
         orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
 
+        if(Input.GetKeyDown(KeyCode.E)) PlayerInteract();
+    }
+    public void PlayerInteract()
+    {
+        var layermask0 = 1 << 0;
+        var layermask3 = 1 << 3;
+        var finalmask = layermask0 | layermask3;
+        
+        RaycastHit hit;
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(5f, 5f, 0));
+
+        if (Physics.Raycast(ray, out hit, 15, finalmask))
+        {
+            Interact interactScript = hit.transform.GetComponent<Interact>();
+            if (interactScript) interactScript.CallInteract(this);
+        }
     }
 }
