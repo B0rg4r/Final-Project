457961b82cb1ee01b0d5603498f6326342cdb1d0
itsducksss <152821176+itsducksss@users.noreply.github.com>
2026-06-16 17:11:40 +0100

    Player health
    changes:
    - modified the script so you are able to take damage after colliding with the enemy (issue must walk inside of the enemy for damage to take effect)
    - made is so that if your health hits 0 the scene resets

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,8 +1,11 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
+    public Slider healthBar;
     public GameObject player; 
     public int health;
     public int maxHealth = 10;
@@ -10,26 +13,28 @@ public class PlayerStats : MonoBehaviour
     private void Start()
     {
         health = maxHealth;
+        healthBar.value = 10;
     }
-    
-    public void TakeDamage(int amount)
-    {
-        health -= amount;
-    }
-
-   
-
     private void Update()
     {
+        healthBar.value = health;
         if (health <= 0)
         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             Debug.Log("Player is dead");
-             
         }
     }
 
+    //public void TakeDamage(int amount)
+    //{
+    //    health -= amount;
+    //}
 
-
-
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            health -= 1;
+        }
+    }
 }
